1e8d6eb32fe2bf83319067110f0fded63ca6b1e2
harish <harry.softer@gmail.com>
2015-11-03 21:18:17 +0530

    popularing knowledge in state based on entities and indexes created.

--- state.js
+++ state.js
@@ -3,7 +3,7 @@ let { queryToKeyrange, keyrangeToQuery } = require("./querykeyrange"), Index = r
 
 module.exports = class State {
 	constructIndex(state) {
-		for (key in this.knowledge) {
+		for (key in state.knowledge) {
 			let query = keyrangeToquery(key, "");
 			this.indexes[key] = new Index();
 			this.indexes[key].setQuery(query);
@@ -17,11 +17,29 @@ module.exports = class State {
 			}
 		}
 	}
-
+	
+	populateKnowledge() {
+		for(key in this.indexes) {
+			let index = this.indexes[key];
+			this.knowledge[key] = [];
+			if(index.length) continue;
+			this.knowledge[key].push({
+				start: index[0][index.getQuery().order],
+				end: index[0][index.getQuery().order]
+			});
+		}
+	}
+	
 	constructor(initialState) {
 		this.entities = new WeakMap();
 		this.knowledge = {};
 		this.indexes = {};
+		for(i in initialState.entities) {
+			if(initialState.hasOwnProperty(i)) {
+				this.entities[i] = initialState.entities[i]
+			}
+		}
 		constructIndex(initialState);
+		populateKnowledge();
 	}
 };
