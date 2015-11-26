19cf330c9843dabce4ccb7e1f7a45399186d3b79
harish <harry.softer@gmail.com>
2015-11-26 19:48:33 +0530

    better grouping of tests.

--- test/cache.js
+++ test/cache.js
@@ -5,21 +5,21 @@ let Cache = require("../lib/Cache"),
 	assert = require("assert"),
 	cache;
 
-beforeEach(() => {
-	cache = new Cache();
-});
-
-it("should insert a single entity", function () {
+describe("should insert a single entity", function () {
+	let cache = new Cache();
 	let room = {
 		id: "numix",
 		type: "room",
 		description: "GTK+ and Gnome Shell themes."
 	};
 	cache.put({ entities: { numix: room }});
-	assert.deepEqual(cache.entities, { numix: room });
+	it("checking if the insert was successful", function(){
+		assert.deepEqual(cache.entities, { numix: room });
+	});
 });
 
-it("should insert a new range and query it", function () {
+describe("should insert a new range and query it", function () {
+	let cache = new Cache();
 	cache.put({
 		knowledge: { "room:updateTime(:)": [ [ 0, 7 ] ] },
 		indexes: { "room:updateTime(:)": [
@@ -29,31 +29,40 @@ it("should insert a new range and query it", function () {
 		] }
 	});
 
-	let res = cache.query("room:updateTime(:)", [ 1, 5 ]);
-	assert.equal(res.arr.length, 2, "incorrect results");
-	assert.equal(res.arr[0].updateTime, 1, "incorrect item");
-	assert.equal(res.arr[1].updateTime, 3, "incorrect item");
+	it("simple query", function() {
+		let res = cache.query("room:updateTime(:)", [ 1, 5 ]);
+		assert.equal(res.arr.length, 2, "incorrect results");
+		assert.equal(res.arr[0].updateTime, 1, "incorrect item");
+		assert.equal(res.arr[1].updateTime, 3, "incorrect item");
+	});
 
-	res = cache.query("room:updateTime(:)", [ 1, 9 ]);
-	assert.equal(res.arr.length, 4, "incorrect results");
-	assert.equal(res.arr[0].updateTime, 1, "incorrect item");
-	assert.equal(res.arr[1].updateTime, 3, "incorrect item");
-	assert.equal(res.arr[2].updateTime, 6, "incorrect item");
-	assert.equal(res.arr[3].updateTime, 7, "incorrect item");
-	assert.equal(res.arr[3].type, "loading", "incorrect item");
+	it("query with loading at the end", function() {
+		let res = cache.query("room:updateTime(:)", [ 1, 9 ]);
+		assert.equal(res.arr.length, 4, "incorrect results");
+		assert.equal(res.arr[0].updateTime, 1, "incorrect item");
+		assert.equal(res.arr[1].updateTime, 3, "incorrect item");
+		assert.equal(res.arr[2].updateTime, 6, "incorrect item");
+		assert.equal(res.arr[3].updateTime, 7, "incorrect item");
+		assert.equal(res.arr[3].type, "loading", "incorrect item");
+	});
 
-	res = cache.query("room:updateTime(:)", [ 3, 0, 1 ]);
-	assert.equal(res.arr.length, 1, "incorrect results");
-	assert.equal(res.arr[0].updateTime, 3, "incorrect item");
+	it("query with 3 property ranges with after only single item", function() {
+		let res = cache.query("room:updateTime(:)", [ 3, 0, 1 ]);
+		assert.equal(res.arr.length, 1, "incorrect results");
+		assert.equal(res.arr[0].updateTime, 3, "incorrect item");
+	});
 
-	res = cache.query("room:updateTime(:)", [ 1, 0, 3 ]);
-	assert.equal(res.arr.length, 3, "incorrect results");
-	assert.equal(res.arr[0].updateTime, 1, "incorrect item");
-	assert.equal(res.arr[1].updateTime, 3, "incorrect item");
-	assert.equal(res.arr[2].updateTime, 6, "incorrect item");
+	it("query with 3 property ranges with after only: multiple items with no loading", function() {
+		let res = cache.query("room:updateTime(:)", [ 1, 0, 3 ]);
+		assert.equal(res.arr.length, 3, "incorrect results");
+		assert.equal(res.arr[0].updateTime, 1, "incorrect item");
+		assert.equal(res.arr[1].updateTime, 3, "incorrect item");
+		assert.equal(res.arr[2].updateTime, 6, "incorrect item");
+	});
 });
 
-it.only("should insert and query a new range which has most recent data", function () {
+describe("query for cache with infinity in the end", function() {
+	let cache = new Cache();
 	cache.put({
 		knowledge: { "relation:roleTime(:)": [ [ 0, +Infinity ] ] },
 		indexes: { "relation:roleTime(:)": [
@@ -63,29 +72,34 @@ it.only("should insert and query a new range which has most recent data", functi
 		] }
 	});
 
-	console.log(cache.knowledge);
-	let res = cache.query("relation:roleTime(:)", [ 1, 9 ]);
-	console.log("all items reading: done", res);
-	assert.equal(res.arr[0].roleTime, 1, "incorrect item");
-	assert.equal(res.arr[1].roleTime, 3, "incorrect item");
-	assert.equal(res.arr[2].roleTime, 6, "incorrect item");
+	it("query with 2 prop ranges", function () {
+		let res = cache.query("relation:roleTime(:)", [ 1, 9 ]);
+		assert.equal(res.arr[0].roleTime, 1, "incorrect item");
+		assert.equal(res.arr[1].roleTime, 3, "incorrect item");
+		assert.equal(res.arr[2].roleTime, 6, "incorrect item");
+	});
 
-	res = cache.query("relation:roleTime(:)", [ 3, 0, 3 ]);
-	assert.equal(res.arr.length, 2, "incorrect results");
-	assert.equal(res.arr[0].roleTime, 3, "incorrect item");
-	assert.equal(res.arr[1].roleTime, 6, "incorrect item");
-
-	res = cache.query("relation:roleTime(:)", [ 3, 2, 3 ]);
-	assert.equal(res.arr.length, 4, "incorrect results");
-	assert.equal(res.arr[0].type, "loading", "incorrect item");
-	assert.equal(res.arr[1].roleTime, 1, "incorrect item");
-	assert.equal(res.arr[2].roleTime, 3, "incorrect item");
-	assert.equal(res.arr[3].roleTime, 6, "incorrect item");
-	assert(Object.keys(cache.queries), "no queries added");
+	it("query with 3 prop ranges: shouldn't return loading at the end.", function () {
+		let res = cache.query("relation:roleTime(:)", [ 3, 0, 3 ]);
+		assert.equal(res.arr.length, 2, "incorrect results");
+		assert.equal(res.arr[0].roleTime, 3, "incorrect item");
+		assert.equal(res.arr[1].roleTime, 6, "incorrect item");
+	});
+	it("query with 3 prop ranges: should give loading in the beginning and add a query.", function () {
+		let res = cache.query("relation:roleTime(:)", [ 3, 2, 3 ]);
+		assert.equal(res.arr.length, 4, "length not correct on ");
+		assert.equal(res.arr[0].type, "loading", "incorrect item");
+		assert.equal(res.arr[1].roleTime, 1, "incorrect item");
+		assert.equal(res.arr[2].roleTime, 3, "incorrect item");
+		assert.equal(res.arr[3].roleTime, 6, "incorrect item");
+		assert(Object.keys(cache.queries), "no queries added");
+	});
 });
 
 
-it("should index entities when adding knowledge: ", function () {
+describe.only("should index entities when adding knowledge: ", function () {
+	let cache = new Cache();
+	console.log("starting.");
 	cache.put({
 		entities: {
 			harish_numix: { id: "harish_numix", roleTime: 1 },
@@ -93,14 +107,16 @@ it("should index entities when adding knowledge: ", function () {
 			satya_numix: { id: "satya_numix", roleTime: 6 }
 		}
 	});
-
-	cache.put({
+/*	cache.put({
 		knowledge: { "relation:roleTime(:)": [ [ 0, +Infinity ] ] }
+	});*/
+	it("adding knowledge should add the indexes", function() {
+		assert(Object.keys(cache.indexes).length, "no index added");
 	});
-	console.log(JSON.stringify(cache.indexes));
 });
 
-it.only("deleting an item which is part of an index: ", function () {
+describe("deleting an item which is part of an index: ", function () {
+	let cache = new Cache();
 	cache.put({
 		knowledge: { "room:updateTime(:)": [ [ 0, 7 ] ] },
 		indexes: { "room:updateTime(:)": [
@@ -113,9 +129,11 @@ it.only("deleting an item which is part of an index: ", function () {
 	cache.put({
 		numix: null
 	});
-	let res = cache.query("room:updateTime(:)", [ 1, 5 ]);
-	console.log(res);
-	assert.equal(res.arr[0].id, "numix", "didnt delete");
+
+	it("adding knowledge should add the indexes", function() {
+		let res = cache.query("room:updateTime(:)", [ 1, 5 ]);
+		assert.equal(res.arr[0].id, "numix", "didnt delete");
+	});
 });
 
 /*
