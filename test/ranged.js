89a6c1b71ac119ff8fc8d86419e5e8ff183c5f8c
harish <harry.softer@gmail.com>
2016-02-18 17:32:08 +0530

    Hooray, tests are passing for ranges.

--- test/ranged.js
+++ test/ranged.js
@@ -4,57 +4,92 @@ let RangeArray = require('../lib/RangeArray'),
 	assert = require('assert');
 
 it('should subtract a whole range', () => {
-	assert.deepEqual(
-		new RangeArray([ [ 1, 4 ], [ 6, 9 ] ]).difference(new RangeArray([ [ 1, 4 ] ])),
-		{ arr: [ [ 6, 9 ] ]}
-	);
+	let rangeArray = new RangeArray([
+		{start: 1, end: 4}, {start: 6, end: 9}
+	]).difference(new RangeArray([ {start: 1, end: 4} ]));
+	assert(rangeArray instanceof RangeArray);
+	assert.equal(rangeArray.arr.length, 1, 'length incorrect');
+	assert.equal(rangeArray.arr[0].start, 6, 'first item start incorrect');
+	assert.equal(rangeArray.arr[0].end, 9, 'first item end incorrect');
+});
+it('should subtract part of ranges', () => {
+	let rangeArray = new RangeArray([ {start: 1, end: 4}, {start: 6, end: 9} ]).difference(new RangeArray([ {start: 3, end: 7 } ]));
+
+	assert(rangeArray instanceof RangeArray);
+	assert.equal(rangeArray.arr.length, 2, 'length incorrect');
+	assert.equal(rangeArray.arr[0].start, 1, 'first item start incorrect');
+	assert.equal(rangeArray.arr[0].end, 3, 'first item end incorrect');
+	assert.equal(rangeArray.arr[1].start, 7, 'second item start incorrect');
+	assert.equal(rangeArray.arr[1].end, 9, 'second item end incorrect');
 });
 
-it('should subtract part of ranges', () => {
-	assert.deepEqual(
-		new RangeArray([ [ 1, 4 ], [ 6, 9 ] ]).difference(new RangeArray([ [ 3, 7 ] ])),
-		{ arr: [ [ 1, 3 ], [ 7, 9 ] ]}
-	);
-});
 
 it('should add an overlapping 3-range at the end', () => {
-	let ra = new RangeArray([ [ 1, 4 ] ]);
-	ra.add([ 3, 5 ]);
-	assert.deepEqual(ra, { arr: [ [ 1, 5 ] ]});
+	let rangeArray = new RangeArray([ {start: 1, end: 4} ]);
+
+	rangeArray.add({start: 3, end: 5});
+	assert(rangeArray instanceof RangeArray);
+	assert.equal(rangeArray.arr.length, 1, 'length incorrect');
+	assert.equal(rangeArray.arr[0].start, 1, 'first item start incorrect');
+	assert.equal(rangeArray.arr[0].end, 5, 'first item end incorrect');
 });
 
 it('should add a non-overlapping 3-range at the end', () => {
-	let ra = new RangeArray([ [ 1, 4 ] ]);
-	ra.add([ 5, 7 ]);
-	assert.deepEqual(ra, { arr: [ [ 1, 4 ], [ 5, 7 ] ]});
+	let rangeArray = new RangeArray([ {start: 1, end: 4} ]);
+
+	rangeArray.add({start: 5, end: 7});
+	assert(rangeArray instanceof RangeArray);
+	assert.equal(rangeArray.arr.length, 2, 'length incorrect');
+	assert.equal(rangeArray.arr[0].start, 1, 'first item start incorrect');
+	assert.equal(rangeArray.arr[0].end, 4, 'first item end incorrect');
+	assert.equal(rangeArray.arr[1].start, 5, 'second item start incorrect');
+	assert.equal(rangeArray.arr[1].end, 7, 'second item end incorrect');
 });
 
 
 it('should add a touching 3-range at the end', () => {
-	let ra = new RangeArray([ [ 1, 4 ] ]);
-	ra.add([ 4, 7 ]);
-	assert.deepEqual(ra, { arr: [ [ 1, 7 ] ]});
+	let rangeArray = new RangeArray([ {start: 1, end: 4} ]);
+
+	rangeArray.add({start: 4, end: 7});
+	assert(rangeArray instanceof RangeArray);
+	assert.equal(rangeArray.arr.length, 1, 'length incorrect');
+	assert.equal(rangeArray.arr[0].start, 1, 'second item start incorrect');
+	assert.equal(rangeArray.arr[0].end, 7, 'second item end incorrect');
 });
 
 it('should add an overlapping 3-range at the beginning', () => {
-	let ra = new RangeArray([ [ 1, 4 ] ]);
-	ra.add([ 0, 2 ]);
-	assert.deepEqual(ra, { arr: [ [ 0, 4 ] ]});
+	let rangeArray = new RangeArray([ {start: 1, end: 4} ]);
+
+	rangeArray.add({start: 0, end: 2});
+	assert(rangeArray instanceof RangeArray);
+	assert.equal(rangeArray.arr.length, 1, 'length incorrect');
+	assert.equal(rangeArray.arr[0].start, 0, 'first item start incorrect');
+	assert.equal(rangeArray.arr[0].end, 4, 'first item end incorrect');
 });
 
 it('should add a non-overlapping 3-range at the beginning', () => {
-	let ra = new RangeArray([ [ 1, 4 ] ]);
-	ra.add([ -1, 0 ]);
-	assert.deepEqual(ra, { arr: [ [ -1, 0 ], [ 1, 4 ] ]});
+	let rangeArray = new RangeArray([ {start: 1, end: 4} ]);
+
+	rangeArray.add({start: -1, end: 0});
+	assert(rangeArray instanceof RangeArray);
+	assert.equal(rangeArray.arr.length, 2, 'length incorrect');
+	assert.equal(rangeArray.arr[0].start, -1, 'first item start incorrect');
+	assert.equal(rangeArray.arr[0].end, 0, 'first item end incorrect');
+	assert.equal(rangeArray.arr[1].start, 1, 'second item start incorrect');
+	assert.equal(rangeArray.arr[1].end, 4, 'second item end incorrect');
 });
 
-it('should add a touching 3-range at the beginning', () => {
-	let ra = new RangeArray([ [ 1, 4 ] ]);
-	ra.add([ 0, 1 ]);
-	assert.deepEqual(ra, { arr: [ [ 0, 4 ] ]});
+it('should add a non-overlapping 3-range at the beginning', () => {
+	let rangeArray = new RangeArray([ {start: 1, end: 4} ]);
+
+	rangeArray.add({start: 0, end: 1});
+	assert(rangeArray instanceof RangeArray);
+	assert.equal(rangeArray.arr.length, 1, 'length incorrect');
+	assert.equal(rangeArray.arr[0].start, 0, 'first item start incorrect');
+	assert.equal(rangeArray.arr[0].end, 4, 'first item end incorrect');
 });
 
 it('should JSONify', () => {
-	let ra = new RangeArray([ [ 1, 4 ] ]);
-	console.log(JSON.stringify(ra));
+	let rangeArray = new RangeArray([ {start: 1, end: 4} ]);
+	assert(JSON.stringify(rangeArray), 'hooray!!!');
 });
