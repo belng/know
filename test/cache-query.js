03eb2d4ac4a820b567a2b812785ba956faf6385c
aravind <aravindet@gmail.com>
2016-03-05 14:09:42 +0530

    Add query clamping at Infinity

---
+++ test/cache-query.js
@@ -0,0 +1,33 @@
+/* eslint-env mocha */
+/* eslint no-console:0 */
+'use strict';
+
+require('babel-core/register')({sourceMaps: 'inline'});
+
+let Cache = require('../lib/Cache'),
+	RangeArray = Cache.RangeArray,
+	assert = require('assert');
+
+it('-Infinity queries should be clamped', () => {
+	let cache = new Cache();
+
+	cache.onChange(change => {
+		assert.deepEqual(change.queries['text:createTime'],
+			new RangeArray([ [ -Infinity, 0, 10 ] ])
+		);
+	});
+
+	cache.query('text:createTime', [ -Infinity, 10, 10 ]);
+});
+
+it('+Infinity queries should be clamped', () => {
+	let cache = new Cache();
+
+	cache.onChange(change => {
+		assert.deepEqual(change.queries['text:createTime'],
+			new RangeArray([ [ Infinity, 10, 0 ] ])
+		);
+	});
+
+	cache.query('text:createTime', [ Infinity, 10, 10 ]);
+});
