f62fabd8115c637e3dedcac680b986dc66357086
Satyajit Sahoo <satyajit.happy@gmail.com>
2016-03-15 02:03:01 +0530

    Update tests for State

--- test/state-test.js
+++ test/state-test.js
@@ -3,55 +3,78 @@
 
 'use strict';
 
-const State = require('../lib/State'),
+const State = require('../lib/State').default,
 	  assert = require('assert');
 
 describe('State', () => {
-	it('should put, watch and get data', () => {
-		const state = new State({
-			op: {}
-		});
+	it('should set and get data', () => {
+		const state = new State();
 
-		let sub1 = -1,
-			sub2 = -1;
+		state.set('mydata', [ 3, 4, 5 ]);
 
-		const unwatch1 = state.watch([ 'something' ], result => {
-			sub1++;
+		assert.deepEqual(state.get('mydata'), [ 3, 4, 5 ], `result doesn't match the current data`);
+	});
 
-			switch (sub1) {
+	it('should put and watch', () => {
+		const state = new State();
+
+		let sub = -1;
+
+		state.watch('mydata', result => {
+			sub++;
+
+			switch (sub) {
 			case 0:
 				return assert.equal(result, undefined, `result doesn't match the current data`);
 			case 1:
-				return assert.equal(result, 'test-1', `result doesn't match the current data`);
-			case 2:
-				return assert.fail(result, null, 'callback called after being unwatched');
+				return assert.deepEqual(result, [ 3, 4, 5 ], `result doesn't match the current data`);
 			}
 		});
 
 		state.put({
-			something: 'test-1'
+			mydata: [ 3, 4, 5 ]
 		});
+	});
 
-		const unwatch2 = state.watch([ 'something' ], result => {
-			sub2++;
+	it('should not fire after unwatch', () => {
+		const state = new State();
 
-			switch (sub2) {
-			case 0:
-				return assert.equal(result, 'test-1', `result doesn't match the current data`);
-			case 1:
-				return assert.fail(result, null, 'callback called after being unwatched');
+		let sub = -1;
+
+		const unwatch = state.watch('mydata', result => {
+			sub++;
+
+			if (sub > 1) {
+				assert.fail(result, null, 'callback called after being unwatched');
 			}
 		});
 
-		assert.equal(state.get([ 'something' ]), 'test-1', `result doesn't match the current data`);
-
-		unwatch1();
-		unwatch2();
-
 		state.put({
-			something: 'test-2'
+			mydata: [ 3, 4, 5 ]
 		});
 
-		assert.equal(state.get([ 'something' ]), 'test-2', `result doesn't match the current data`);
+		unwatch();
+	});
+
+	it('should not fire if value didn\'t change', () => {
+		const state = new State();
+
+		state.put({
+			mydata: 'hello'
+		});
+
+		let sub = 0;
+
+		state.watch('mydata', result => {
+			if (sub > 0) {
+				assert.fail(result, null, 'callback called without value changing');
+			}
+
+			sub++;
+		});
+
+		state.put({
+			mydata: 'hello'
+		});
 	});
 });
