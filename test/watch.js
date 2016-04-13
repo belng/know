3bb654ece09207f5341c7d94ed1fd4c56cac376c
Satyajit Sahoo <satyajit.happy@gmail.com>
2016-04-13 19:37:31 +0530

    Add test for watch when entity in a list changes

---
+++ test/watch.js
@@ -0,0 +1,65 @@
+/* eslint-env mocha */
+/* eslint no-console:0 */
+'use strict';
+
+require('babel-core/register')({sourceMaps: 'inline'});
+
+let Cache = require('../lib/Cache').default,
+	RangeArray = Cache.RangeArray,
+	OrderedArray = Cache.OrderedArray;
+
+it.only('should fire onchange when one entity in list changes', (done) => {
+	const cache = new Cache({});
+
+	cache.put({
+		knowledge: {
+			'text:createTime': new RangeArray([ [ 1, 3 ] ])
+		},
+		indexes: {
+			'text:createTime': new OrderedArray(
+				[ 'createTime' ], [
+					{
+						createTime: 1,
+						type: 'text',
+						body: 'hey',
+						id: 'a'
+					},
+					{
+						createTime: 2,
+						type: 'text',
+						body: 'ho',
+						id: 'b'
+					},
+					{
+						createTime: 3,
+						type: 'text',
+						body: 'go',
+						id: 'c'
+					}
+				]
+			)
+		}
+	});
+
+	cache.watch({
+		type: 'text',
+		order: 'createTime',
+	}, [ -Infinity, +Infinity ], (data) => {
+		if (data.arr[3].body === 'some') {
+			done();
+		}
+	});
+
+	setTimeout(() => {
+		cache.put({
+			entities: {
+				c: {
+					createTime: 3,
+					type: 'text',
+					body: 'some',
+					id: 'c'
+				}
+			}
+		});
+	}, 10);
+});
