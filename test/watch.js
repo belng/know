ef6132227f1f9f676fa3d85524ec3bf0d4ed8b3c
Satyajit Sahoo <satyajit.happy@gmail.com>
2016-04-20 20:14:38 +0530

    test(watch): add falling test for cache.watch

--- test/watch.js
+++ test/watch.js
@@ -4,7 +4,8 @@
 
 require('babel-core/register')({sourceMaps: 'inline'});
 
-let Cache = require('../lib/Cache').default,
+let assert = require('assert'),
+	Cache = require('../lib/Cache').default,
 	RangeArray = Cache.RangeArray,
 	OrderedArray = Cache.OrderedArray;
 
@@ -62,4 +63,84 @@ it('should fire onchange when one entity in list changes', (done) => {
 			}
 		});
 	}, 10);
+});
+
+it('should fire watch with correct number of results', (done) => {
+	const cache = new Cache({
+		is: () => true,
+		id: entity => entity.id,
+	});
+
+	cache.put({
+		knowledge: { 'text:createTime!(parents~Scts:(9929e4d6~F39d2~F47c7~Fb595~F6f38729b1a34))': new RangeArray(
+			[ [ -Infinity, +Infinity ] ]
+		) },
+		indexes: { 'text:createTime!(parents~Scts:(9929e4d6~F39d2~F47c7~Fb595~F6f38729b1a34))': new OrderedArray(
+			[ 'createTime' ],
+			[]
+		) }
+	});
+
+	let i = 0;
+
+	cache.watch({
+		type: 'text',
+		filter: {
+			parents_cts: [ '9929e4d6-39d2-47c7-b595-6f38729b1a34' ],
+		},
+		order: 'createTime',
+	}, [ Infinity, 20, 0 ], results => {
+		if (i === 0) {
+			assert.equal(0, results.arr.length);
+		}
+
+		if (i === 1) {
+			assert.equal(1, results.arr.length);
+		}
+
+		if (i === 2) {
+			assert.equal(2, results.arr.length);
+			done();
+		}
+
+		i++;
+	});
+
+	setTimeout(() =>
+		cache.put({
+			entities: {
+				'7c81733b-a28a-484d-a152-91aa3ecfb6f6': {
+					body: 'Test 1',
+					createTime: 1461159560709,
+					creator: 'satya164',
+					id: '7c81733b-a28a-484d-a152-91aa3ecfb6f6',
+					parents: [
+						'9929e4d6-39d2-47c7-b595-6f38729b1a34',
+						'0fc369e4-fbd9-4592-973b-82d9a4b17967'
+					],
+					tags: [],
+					type: 2,
+					updateTime: 1461159560709
+				}
+			}
+		}), 100);
+
+	setTimeout(() =>
+		cache.put({
+			entities: {
+				'7c81733b-a28a-484d-a152-91aa3ecfb67': {
+					body: 'Test 2',
+					createTime: 1461159560710,
+					creator: 'satya164',
+					id: '7c81733b-a28a-484d-a152-91aa3ecfb6f7',
+					parents: [
+						'9929e4d6-39d2-47c7-b595-6f38729b1a34',
+						'0fc369e4-fbd9-4592-973b-82d9a4b17967'
+					],
+					tags: [],
+					type: 2,
+					updateTime: 1461159560710
+				}
+			}
+		}), 300);
 });
