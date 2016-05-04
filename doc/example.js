f839991379f78ba5e6049badc371cec09ddaeb23
Satyajit Sahoo <satyajit.happy@gmail.com>
2016-05-04 06:05:11 +0530

    Fix ESLint warnings

--- doc/example.js
+++ doc/example.js
@@ -2,26 +2,26 @@
 	Example: usage of cache in pure server-side store.
 */
 
-let Cache = require("cache"),
-	constants = require("lib/constants"),
+let Cache = require('cache'),
+	constants = require('lib/constants'),
 	typeMap = {
-		[constants.TYPE_ROOM]: {room: true, item: true},
-		[constants.TYPE_TEXT]: {text: true, item: true},
-		[constants.TYPE_USER]: {user: true}
+		[constants.TYPE_ROOM]: { room: true, item: true },
+		[constants.TYPE_TEXT]: { text: true, item: true },
+		[constants.TYPE_USER]: { user: true }
 	},
 	cache = new Cache({
 		is (entity, type) {
 			if (entity.type) {
 				return !!typeMap[entity.type][type];
 			} else {
-				return type === "relation";
+				return type === 'relation';
 			}
 		},
 		id (entity) {
 			if (entity.id) {
 				return entity.id;
 			} else {
-				return entity.room + ":" + entity.user
+				return entity.room + ':' + entity.user;
 			}
 		}
 	});
@@ -36,11 +36,11 @@ cache.onChange(changes => core.emit(changes));
 cache.put({
 	entities: {
 		test: {
-			id: "test",
-			type: "room",
+			id: 'test',
+			type: 'room',
 			createTime: 1
 		}
 	}
 });
 
-assert(cache.entities.test.id == "test");
+assert(cache.entities.test.id == 'test');
