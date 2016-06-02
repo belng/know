2f29040789c5f714eb24f98084ce6901aa0b3ef3
aravind <aravindet@gmail.com>
2016-06-02 22:12:51 +0530

    Fix indexing when join secondary is pushed

--- tests/compound-join.test.js
+++ tests/compound-join.test.js
@@ -100,4 +100,48 @@ test.serial('query', t => {
 	));
 });
 
-test.serial.todo('push');
+test('push', t => {
+	const cache = new Cache(); //eslint-disable-line
+	const key = 'item+(rel:item):score';
+	cache.put({
+		knowledge: { [key]: [ [ -Infinity, Infinity ] ] },
+		indexes: { [key]: [
+			{ item: { id: 'item1', type: 'item', score: 1 }, rel: { id: 'me_item1', type: 'rel', role: 1, item: 'item1' } },
+			{ item: { id: 'item2', type: 'item', score: 1 }, rel: { id: 'me_item2', type: 'rel', role: 1, item: 'item2' } },
+			{ item: { id: 'item3', type: 'item', score: 1 }, rel: { id: 'me_item3', type: 'rel', role: 1, item: 'item3' } },
+			{ item: { id: 'item4', type: 'item', score: 1 }, rel: { id: 'me_item4', type: 'rel', role: 1, item: 'item4' } }
+		] }
+	});
+
+	t.deepEqual(
+		cache.query(key, [ -Infinity, Infinity ]).arr,
+		[ { item: { id: 'item4', type: 'item', score: 1 },
+		    rel: { id: 'me_item4', type: 'rel', role: 1, item: 'item4' } },
+		  { item: { id: 'item2', type: 'item', score: 1 },
+		    rel: { id: 'me_item2', type: 'rel', role: 1, item: 'item2' } },
+		  { item: { id: 'item3', type: 'item', score: 1 },
+		    rel: { id: 'me_item3', type: 'rel', role: 1, item: 'item3' } },
+		  { item: { id: 'item4', type: 'item', score: 1 },
+		    rel: { id: 'me_item4', type: 'rel', role: 1, item: 'item4' } }
+		]
+	);
+
+	cache.put({
+		entities: {
+			me_item2: { role: 2 }
+		}
+	});
+
+	t.deepEqual(
+		cache.query(key, [ -Infinity, Infinity ]).arr,
+		[ { item: { id: 'item4', type: 'item', score: 1 },
+			rel: { id: 'me_item4', type: 'rel', role: 1, item: 'item4' } },
+		  { item: { id: 'item2', type: 'item', score: 1 },
+			rel: { id: 'me_item2', type: 'rel', role: 2, item: 'item2' } },
+		  { item: { id: 'item3', type: 'item', score: 1 },
+			rel: { id: 'me_item3', type: 'rel', role: 1, item: 'item3' } },
+		  { item: { id: 'item4', type: 'item', score: 1 },
+			rel: { id: 'me_item4', type: 'rel', role: 1, item: 'item4' } }
+		]
+	);
+});
