48fe9e48b7bd729bbed307742c08e52ba73e4309
Satyajit Sahoo <satyajit.happy@gmail.com>
2016-06-03 14:32:12 +0530

    Add test for inserting new primary item

--- tests/compound-join.test.js
+++ tests/compound-join.test.js
@@ -145,3 +145,32 @@ test('push', t => {
 		]
 	);
 });
+
+test.skip('new primary item', t => {
+	const cache = new Cache(); //eslint-disable-line
+	const key = 'item+(rel:item):score';
+	cache.put({
+		knowledge: { [key]: [ [ -Infinity, Infinity ] ] },
+		indexes: { [key]: [
+			{ item: { id: 'item1', type: 'item', score: 1 }, rel: { id: 'me_item1', type: 'rel', role: 1, item: 'item1' } },
+			{ item: { id: 'item2', type: 'item', score: 1 }, rel: { id: 'me_item2', type: 'rel', role: 1, item: 'item2' } },
+		] }
+	});
+
+	cache.put({
+		entities: {
+			me_item5: { id: 'item4', type: 'item', score: 1 }
+		}
+	});
+
+	t.deepEqual(
+		cache.query(key, [ -Infinity, Infinity ]).arr,
+		[ { item: { id: 'item4', type: 'item', score: 1 },
+			rel: { id: 'me_item4', type: 'rel', role: 1, item: 'item4' } },
+		  { item: { id: 'item2', type: 'item', score: 1 },
+			rel: { id: 'me_item2', type: 'rel', role: 2, item: 'item2' } },
+		  { item: { id: 'item5', type: 'item', score: 1 },
+			rel: null }
+		]
+	);
+});
