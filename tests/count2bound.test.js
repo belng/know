0067ef749c2b3241dfc7ca78153ec06ec8473036
aravind <aravindet@gmail.com>
2016-06-06 02:02:43 +0530

    (refactor) Move static functions out of cache

--- tests/count2bound.test.js
+++ tests/count2bound.test.js
@@ -1,17 +1,12 @@
 import test from 'ava';
-import Cache from '../lib/Cache';
-
-const {
-	RangeArray,
-	OrderedArray,
-} = Cache;
+import RangeArray from '../lib/RangeArray';
+import OrderedArray from '../lib/OrderedArray';
 
 test('should handle infinities correctly', t => {
-	let cache = new Cache();
 	t.deepEqual(
-		cache.countedToBounded(
+		RangeArray.countedToBounded(
 			RangeArray.makeRange([ Infinity, 3, 0 ]),
-			new OrderedArray([ "createTime" ], [
+			new OrderedArray([ 'createTime' ], [
 				{ id: 'blahblah1', type: 'text', createTime: 21 },
 				{ id: 'blahblah2', type: 'text', createTime: 22 },
 				{ id: 'blahblah3', type: 'text', createTime: 23 },
@@ -25,8 +20,7 @@ test('should handle infinities correctly', t => {
 });
 
 test('should return infinity for empty', t => {
-	let cache = new Cache();
-	t.deepEqual(cache.countedToBounded(
+	t.deepEqual(RangeArray.countedToBounded(
 		RangeArray.makeRange([ Infinity, 3, 0 ]),
 		new OrderedArray([ 'asdf' ], [])
 	), new RangeArray(
